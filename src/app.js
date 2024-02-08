c0f6e4fd592bb4ae86b9321cc3a002803a17d3bd
GhostGirl03 <152809822+GhostGirl03@users.noreply.github.com>
2024-02-08 01:18:03 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,24 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+const who = ["My dog", "My landlord", "My grandma", "My mother in law"];
+const action = ["ate", "peed", "crushed", "broke"];
+const what = ["my homework", "the keys", "the car"];
+const when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  console.log("Hello MDC-25");
+
+  //TODO: Create a random excuse
+  const excuse = who[0] + " " + action[1] + " " + what[2] + " " + when[3];
+
+  //Change the HTML page to show the random excuse
+  document.querySelector("div").innerText = excuse;
 };
